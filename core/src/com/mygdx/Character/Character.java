d476a7b7256eb12ae4409a2b9f781afed80a0808
Arthur Pascal <arthur.pascal33@gmail.com>
2023-11-10 02:46:12 +0100

    Feat: fight done

--- core/src/com/mygdx/Character/Character.java
+++ core/src/com/mygdx/Character/Character.java
@@ -5,13 +5,32 @@ import com.mygdx.game.Tile;
 import com.mygdx.item.Weapon;
 
 import java.util.Map;
+
 public class Character {
     private String name;
     private Map<Stat, Integer> stat;
 
+    public void setStat(Map<Stat, Integer> stat) {
+        this.stat = stat;
+    }
+
+    public void setInFight(boolean inFight) {
+        isInFight = inFight;
+    }
+
     private Weapon weaponEquiped;
 
-    public Tile position;
+    private Tile position;
+
+    public boolean isInFight() {
+        return isInFight;
+    }
+
+    public void canFight(boolean inFight) {
+        isInFight = inFight;
+    }
+
+    private boolean isInFight;
 
     public Tile getPosition() {
         return position;
@@ -54,11 +73,34 @@ public class Character {
 
     protected void speak(String string, Character character) {
     }
-    protected boolean attack(Weapon weapon){
-        return false;
+
+    public void changeStat(Stat stat, int value) {
+        this.stat.put(stat, this.stat.get(stat) + value);
     }
 
     public void move(Room room, Tile position, int x, int y) {
         setPosition(position);
+    }
+
+    //TODO : add an miss by luck exception for pretty print
+    public int attack(Character character) {
+        int critMultiplicator = 1;
+        if ((int) (Math.random() * 100) <= getStat().get(Stat.AGILITY)*weaponEquiped.getCriticalStrikeProb()) {
+            System.out.println("Critical strike");
+            critMultiplicator = 2;
+        }
+        if ((int) (Math.random() * 100) <= 100*weaponEquiped.getCriticalFailureProb()) {
+            System.out.println("missed");
+            return 0;
+        } else {
+            int dammage = (int) (getStat().get(Stat.STRENGTH) * weaponEquiped.getDamage() * critMultiplicator);
+            System.out.println("dammage = " + dammage);
+            System.out.println("old HP of "+ character.getName()+"= " + character.getStat().get(Stat.HP));
+
+            character.changeStat(Stat.HP, -dammage);
+            System.out.println("new HP of "+ character.getName()+"= " + character.getStat().get(Stat.HP));
+            return dammage;
+        }
+
     }
 }
